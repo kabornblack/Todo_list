30471facc37f5efb9300082e312542ae8c041dbf
kabornblack <shaibu.kabiru@email.com>
2023-03-17 11:46:27 +0200

    Added list of daily activities and using forloop to add new activities

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Cook", "eat", "read"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req, res){
@@ -18,9 +22,19 @@ app.get("/", function(req, res){
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kingOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+
+});
+
+
+
 
 
 
