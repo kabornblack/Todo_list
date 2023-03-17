b8af4d86f55b11c59b5270ba372b6370f60ceaa7
kabornblack <shaibu.kabiru@email.com>
2023-03-17 09:03:59 +0200

    Formated my date into US standard

--- app.js
+++ app.js
@@ -8,25 +8,15 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", function(req, res){
-  const d = new Date();
-  const days = d.getDay();
-  var day = "";
+  var today  = new Date();
 
-  if (days === 0){
-    day = "Sunday";
-  } else if (days === 1){
-    day = "Monday";
-  } else if (days === 2){
-    day = "Tuesday";
-  } else if (days === 3){
-    day = "Wednesday";
-  } else if (days === 4){
-    day = "Thursday";
-  } else if (days === 5){
-    day = "Friday";
-  } else {
-    day = "Saturday";
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
   };
+
+  var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {kingOfDay: day});
 
