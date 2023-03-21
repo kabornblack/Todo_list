9eae34d0b4dda814e8b43d72ae3e91277cdc7dd4
kabornblack <shaibu.kabiru@email.com>
2023-03-21 11:40:02 +0200

    Created a work page that adds new items to the work list

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Cook", "Eat", "Code", "Read"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
@@ -22,17 +24,27 @@ app.get("/", function(req, res){
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
+
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
 });
 
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
 
 
