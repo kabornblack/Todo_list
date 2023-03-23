91aef2f8bf9cfacfba6af83f5319d3df0d4925d3
kabornblack <shaibu.kabiru@email.com>
2023-03-23 16:19:09 +0200

    Changed all let to const since i am never going to change them

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Cook", "Eat", "Code", "Read"];
-let workItems = ["Write", "Home Work"];
+const items = ["Cook", "Eat", "Code", "Read"];
+const workItems = ["Write", "Home Work"];
 
 app.set('view engine', 'ejs');
 
@@ -16,13 +16,14 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res) {
-  let day = date.getDay();
+  //let day = date.getDay(); to get just only the day from the getDay function
+  const day = date.getDate();
   res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
 
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "work") {
     workItems.push(item);
